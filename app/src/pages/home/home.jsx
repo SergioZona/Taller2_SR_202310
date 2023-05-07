f20b0e68966972a6b87044ad4f6f08763fe3a4a9
Brenda-cbp <bc.barahona@uniandes.edu.co>
2023-05-07 12:30:43 -0500

    listado de lugares dinámico

--- app/src/pages/home/home.jsx
+++ app/src/pages/home/home.jsx
@@ -7,43 +7,33 @@ import { Link, useParams } from 'react-router-dom';
 
 
 function Home() {
+    const list = [1, 2, 3, 4]
+    const recomendations = [];
     const { id } = useParams();
     return (
         <div>
             <Row className="d-flex justify-content-between align-items-center encabezado">
                 <Col>
                     <h1 className="text-center">Lugares</h1>
-
                 </Col>
                 <Col md={{ span: 2 }}>
                     <Link to="/" className="custom-btn btn-10" > Log out</Link>
-
                 </Col>
-
-
             </Row>
 
             <Row>
-                <Col >
-                    <CardPlaces />
-                </Col>
-                <Col>
-                    <CardPlaces />
-                </Col>
-                <Col >
-                    <CardPlaces />
-                </Col>
-                <Col>
-                    <CardPlaces />
-                </Col>
-                <Col >
-                    <CardPlaces />
-                </Col>
-                <Col>
-                    <CardPlaces />
-                </Col>
+                {list ? list.map((l, index) => {
+                    return (<Col key={index}>
+                        <CardPlaces />
+                    </Col>)
+                }) : null}
             </Row>
             <h1 className="text-center">Mis recomendaciones</h1>
+            {recomendations.length !== 0 ? recomendations.map((l, index) => {
+                return (<Col key={index}>
+                    <CardPlaces />
+                </Col>)
+            }) : <div className='no-recomendations'>Aún no tienes recomendaciones. Primero comenta y/o califica lugares :D</div>}
         </div >
     )
 }
